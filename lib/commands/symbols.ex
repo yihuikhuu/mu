f60643408b53160e9e388a9904fb62ba5486ad16
= <yihui.khuu@gmail.com>
2018-03-28 00:51:13 +1030

    Added new grammar; functions not implemented yet

--- lib/commands/symbols.ex
+++ lib/commands/symbols.ex
@@ -49,6 +49,72 @@ defmodule Mu.Commands.Symbols do
       :module => __MODULE__,
       :function => :dot,
       :grammar => :action
+    },
+    "douhao" => %{
+      :description => "comma",
+      :module => __MODULE__,
+      :function => :comma,
+      :grammar => :action
+    },
+    "dengyu" => %{
+      :description => "equal",
+      :module => __MODULE__,
+      :function => :equal,
+      :grammar => :action
+    },
+    "dequal" => %{
+      :description => "double equal",
+      :module => __MODULE__,
+      :function => :double_equal,
+      :grammar => :action
+    },
+    "trequal" => %{
+      :description => "triple equal",
+      :module => __MODULE__,
+      :function => :triple_equal,
+      :grammar => :action
+    },
+    "lequal" => %{
+      :description => "less than or equal",
+      :module => __MODULE__,
+      :function => :less_than_equal,
+      :grammar => :action
+    },
+    "grequal" => %{
+      :description => "greater than or equal",
+      :module => __MODULE__,
+      :function => :greater_than_equal,
+      :grammar => :action
+    },
+    "xiaoguo" => %{
+      :description => "less than",
+      :module => __MODULE__,
+      :function => :less_than,
+      :grammar => :action
+    },
+    "daguo" => %{
+      :description => "greater than",
+      :module => __MODULE__,
+      :function => :greater_than,
+      :grammar => :action
+    },
+    "maohao" => %{
+      :description => "colon",
+      :module => __MODULE__,
+      :function => :colon,
+      :grammar => :action
+    },
+    "fenhao" => %{
+      :description => "semi colon",
+      :module => __MODULE__,
+      :function => :semicolon,
+      :grammar => :action
+    },
+    "wenhao" => %{
+      :description => "question mark",
+      :module => __MODULE__,
+      :function => :question_mark,
+      :grammar => :action
     }
   }
 
